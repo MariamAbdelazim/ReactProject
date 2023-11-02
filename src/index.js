b57b2c3d415a2333efaca341f6e3f3c8c20dd32c
MariamAbdelazim <mariam.abdelazim16@gmail.com>
2023-11-02 19:55:19 +0200

    pages

--- src/index.js
+++ src/index.js
@@ -4,9 +4,40 @@ import './index.css';
 import App from './App';
 
 
+import Home from './pages/home';
+import Doctor from './pages/doctor';
+import Patient from './pages/patient';
+
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>erorr..................</h1> ,
+  },
+  {
+    path: "/doctor",
+    element: <Doctor/>,
+  },
+  {
+    path: "/patient",
+    element: <Patient/>,
+  },
+]);
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
